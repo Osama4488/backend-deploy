8620a2dcfd3295403430f939ca303a059ef74f9f
Osama4488 <syed.osama.ali.96@gmail.com>
2024-01-12 04:22:52 +0500

    second push

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/login", (req, res) => {
+  res.send("<h1>Login Page</h1>");
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
